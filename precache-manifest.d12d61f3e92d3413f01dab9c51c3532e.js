327fe5b75c106081482a319d39731390eac5fc67
NoobieUs37 <NoobieUs37@protonmail.com>
2021-08-26 12:52:31 +0530

    Updates

---
+++ precache-manifest.d12d61f3e92d3413f01dab9c51c3532e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bcb9ada555e0b551705dd4356d9a8ca",
+    "url": "/shoppingapi/index.html"
+  },
+  {
+    "revision": "2d380ade296bd06554ce",
+    "url": "/shoppingapi/static/js/2.745ba322.chunk.js"
+  },
+  {
+    "revision": "013f98f3e6a3f59e6719",
+    "url": "/shoppingapi/static/js/main.fc6c90e0.chunk.js"
+  },
+  {
+    "revision": "4ba262b76f53ab5cc6d3",
+    "url": "/shoppingapi/static/js/runtime-main.5ec1a2f8.js"
+  }
+]);
